185614677b8928afa95760856f635d5458fe1c8d
yuriksensej <yuraminchenko@gmail.com>
2021-01-14 21:34:12 +0200

    tests

---
+++ Test2/script.js
@@ -0,0 +1,12 @@
+let dateV = document.querySelector('[name = "i_4"]');
+let btn_date = document.querySelector('.date_1');
+console.log(btn_date);
+// btn_date.addEventListener('click', console.log(dateV));
+btn_date.onclick = f1;
+function f1() {
+  console.log('hello');
+}
+console.log('1');
+console.log(console.log(2));
+console.log('3');
+console.log('4');
