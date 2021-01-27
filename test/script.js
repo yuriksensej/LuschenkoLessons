a5d5e3ac282bbfaf90796952fe96d042d3c48459
yuriksensej <yuraminchenko@gmail.com>
2021-01-27 23:48:48 +0200

    added task 7 done les 16

---
+++ test/script.js
@@ -0,0 +1,7 @@
+const filterBox = document.querySelectorAll('.box');
+document.querySelector('nav').addEventListener('click', (event) => {
+  if (event.target.tagName !== 'LI') {
+    return false;
+  }
+  let filter;
+});
